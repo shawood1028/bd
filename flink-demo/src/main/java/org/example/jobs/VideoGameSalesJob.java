eff3c4e5fa81e41a511149b586dc2afe27cbb829
Shawood1028 <shawood1028@gmail.com>
2023-01-12 08:21:36 +0800

    init flink-demo

---
+++ flink-demo/src/main/java/org/example/jobs/VideoGameSalesJob.java
@@ -0,0 +1,9 @@
+package org.example.jobs;
+
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+public class VideoGameSalesJob {
+    public static void main(String[] args) {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+    }
+}
